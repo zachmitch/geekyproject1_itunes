ba706499b889d8ee8c32e5754bd2e0ab62232124
Zach Mitchell <zdmitchell@gmail.com>
2015-11-30 08:45:39 -0800

    Foreign territory
    First time seeing Matlab module

--- itunes_duplicate.py
+++ itunes_duplicate.py
@@ -93,16 +93,17 @@ def findDuplicates(fileName):
 
 #- - - - - - - - - - FINDING TRACKS COMMON ACROSS MULTIPLE PLAYLISTS - - - - - - - - - -  - 
 
-
+#pass list of .xml files to findCommonTracks
 def findCommonTracks(fileNames):
 	# a list of sets of track names
 	trackNameSets = []
+	#for each .xml file
 	for fileName in fileNames:
 		#create a new set
 		trackNames = set()
-		#read in playlist
+		#read in playlist fore each .xml file
 		plist = plistlib.readPlist(fileName)
-		#get the tracks
+		#get the tracks from .xml file
 		tracks = plist['Tracks']
 		#iterate through the tracks
 		for trackId, track in tracks.items():
@@ -112,11 +113,11 @@ def findCommonTracks(fileNames):
 			except:
 				#ignore
 				pass
-			#add to list
+			#add individual track list set to sets list
 		trackNameSets.append(trackNames)
 	# get the set of common tracks
 	commonTracks = set.intersection(*trackNameSets)
-	# write to file
+	# write tracks to file
 	if len(commonTracks) > 0:
 		f = open('common.txt', 'w')
 		for val in commonTracks:
@@ -127,3 +128,51 @@ def findCommonTracks(fileNames):
 				'Track names written to common.txt.' % len(commonTracks))
 	else:
 		print('No common tracks!')
+		
+		
+		
+#- - - - - - - - - - - - - - - - COLLECTING STATS-  -- - - - - - - - - - - - - - - 
+
+def plotStats(fileName):
+	#read in the .xml file
+	plist = plistlib.readPlist(fileName)
+	# get the tracks from the playlist
+	tracks = plist['Tracks']
+	# create lists of song ratings and track durations
+	ratings = []
+	durations =[]
+	#iterate through the track list
+	for trackId, track in tracks.items():
+		try:
+			ratings.append(track['Album Rating'])
+			durations.append(track['Total Time'])
+		except:
+			#ignore
+			pass
+	#ensure that valid data was collected
+	if ratings == [] or durations == []:
+		print('No valid Album Rating/Total Time data in %s.' % fileName)
+			return
+
+
+#- - - - - - - - - - - - - - - - PLOTTING STATS - - - - - - - - - - - - - - 
+
+	#scatter plot
+	x = np.array(durations, np.int32)
+	#convert to minutes
+	x = x/60000.0
+	y = np.array(ratings, np.int32)
+	pyplot.subplot(2,1,1)
+	pyplot.plot(x,y, 'o')
+	pyplot.axis([0, 1.05 *np.max(x), -1, 110])
+	pyplot.xlabel('Track duration')
+	pyplot.ylabel('Track rating')
+	
+	#plot historgram
+	pyplot.subplot(2, 1, 2)
+	pyplot.hist(x, bins=20)
+	pyplot.xlabel('Track duration')
+	pyplot.ylabel('Count')
+	
+	#show plot
+	pyplot.show()
